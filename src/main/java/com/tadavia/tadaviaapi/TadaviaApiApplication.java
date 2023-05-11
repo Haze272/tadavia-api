b5f58b11568122facb029e5ef362924857a404b8
Haze272 <studiorain27@gmail.com>
2023-05-11 19:13:36 +0300

    Initial commit

---
+++ src/main/java/com/tadavia/tadaviaapi/TadaviaApiApplication.java
@@ -0,0 +1,13 @@
+package com.tadavia.tadaviaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TadaviaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TadaviaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tadavia/tadaviaapi/TadaviaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tadavia.tadaviaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TadaviaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
